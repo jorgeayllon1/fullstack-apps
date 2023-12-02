0ac26ddcb0ecd31e50d9830dcdb7c65a79440a34
Jorge Ayllon <jorge2640@hotmail.Fr>
2023-12-02 12:41:34 +0100

    Ajout volume pour bdd

--- start-postgresql.sh
+++ start-postgresql.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# init db
 docker run --rm -d \
 --name postgres \
 -e POSTGRES_PASSWORD=postgres \
 -e POSTGRES_USER=postgres \
 -e POSTGRES_DB=dbtest \
 -v ./postgresql/:/docker-entrypoint-initdb.d/ \
+-v pg-data:/var/lib/postgresql/data \
+postgres
+
+# start db
+docker run --rm -d \
+--name postgres \
+-e POSTGRES_PASSWORD=postgres \
+-e POSTGRES_USER=postgres \
+-e POSTGRES_DB=dbtest \
+-v pg-data:/var/lib/postgresql/data \
 postgres
 
 docker run --rm -d \
@@ -15,9 +26,12 @@ docker run --rm -d \
 -p 8080:80 \
 dpage/pgadmin4
 
+# build api
 docker build -f api/Dockerfile.api -t node-api .
+# run api
 docker run --rm -d \
 --name node-api \
+-e PORT=3000 \
 -p 3000:3000 \
 node-api
 
