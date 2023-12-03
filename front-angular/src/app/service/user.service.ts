babaaba297d0ef9154d5cc6f8dbf677dd07e1ac9
Jorge Ayllon <jorge2640@hotmail.Fr>
2023-12-03 20:53:25 +0100

    Docker compose pour postgres et node-api

--- front-angular/src/app/service/user.service.ts
+++ front-angular/src/app/service/user.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
 
+// Externaliser la conf !
 const baseUrl = 'http://localhost:3000'
 
 @Injectable({
